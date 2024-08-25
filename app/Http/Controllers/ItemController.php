74f1255ce46d019e23b63a0bc4982870c8d43058
Ludovic SPINA <ludovic.spina@proton.me>
2024-08-25 16:59:06 +0200

    Initial push

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        $items = Item::all();
+        return response()->json($items);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'string',
+        ]);
+
+        // Create a new post using the validated data
+        $post = Item::create([
+            'name' => $validatedData['name'],
+            'description' => $validatedData['description'],
+            'image' => $validatedData['image'],
+        ]);
+
+        // Return a JSON response with the created resource
+        return response()->json($post);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Item::destroy($id);
+    }
+}
